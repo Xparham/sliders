6441aaa8455178a27d66288fef5af4d14dcb1e91
Xparham <xparham@gmail.com>
2020-12-21 20:15:16 -0500

    Added toggle to show list class

---
+++ app.js
@@ -0,0 +1,8 @@
+const tableMenu = document.getElementById("book-table")
+
+tableMenu.addEventListener('click', function(){
+    // .show-list
+    document.body.classList.toggle('show-list')
+    // tableMenu.classList.add('some-random-class')
+    // console.log(document.body.classList)
+})
