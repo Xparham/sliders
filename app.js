40ee4d3db0a386a68a3dbcd454c0df02f58cce06
Xparham <xparham@gmail.com>
2021-01-11 18:02:04 -0500

    added event listener for span to add hidden class

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const tableMenu = document.getElementById("find-menu")
 const formModal = document.getElementById("form-modal")
 const bookTablebtn = document.getElementById("book-table-btn")
 const closeModal = document.getElementById("close-modal")
+
 tableMenu.addEventListener('click', function(){
     // .show-list
     document.body.classList.toggle('show-list')
@@ -19,5 +20,10 @@ tableMenu.addEventListener('click', function(){
 bookTablebtn.addEventListener('click', function(){
     formModal.classList.remove('hidden')
 
+
+})
+
+closeModal.addEventListener("click", function(){
+    formModal.classList.add('hidden')
 
 })
