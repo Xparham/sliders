58054f6343f0564a252206586e0e7ba81ab58a20
Xparham <xparham@gmail.com>
2021-01-11 17:24:13 -0500

    added element id for book table button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const tableMenu = document.getElementById("find-menu")
 const formModal = document.getElementById("form-modal")
+const bookTablebtn = document.getElementById("book-table-btn")
 
 tableMenu.addEventListener('click', function(){
     // .show-list
